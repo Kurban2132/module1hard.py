066ac2810f7eac0edfa33529a9dc4cadab09e65e
Kurban2132 <k.kokanov@yandex.ru>
2024-07-28 20:28:56 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,31 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+print(grades)
+students = {"Jonny", "bilbo", "Steve", "Khendrik", "Aaron"}
+print(students)
+A = ["Jonny", 5, 3, 3, 5, 4]
+print(A)
+B = ["Bilbo", 2, 2, 2, 3]
+print(B)
+C = ["Steve", 4, 5, 5, 2]
+print(C)
+D = ["Khendrik", 4, 4, 3]
+print(D)
+I = ["Aaron", 5, 5, 5, 4, 5]
+print(I)
+Jonny = [5, 3, 3, 5, 4]
+average = sum(Jonny) / len(Jonny)
+print(average)
+Bilbo = [2, 2, 2, 3]
+average = sum(Bilbo) / len(Bilbo)
+print(average)
+Steve = [4, 5, 5, 2]
+average = sum(Steve) / len(Steve)
+print(average)
+Khendrik = [4, 4, 3]
+average = sum(Khendrik) / len(Khendrik)
+print(average)
+Aaron = [5, 5, 5, 4, 5]
+average = sum(Aaron) / len(Aaron)
+print(average)
+
+
