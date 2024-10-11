8c0c2143e2a658f853cba3ed0fc6af06bcda2008
Kurban2132 <k.kokanov@yandex.ru>
2024-10-11 16:55:01 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,16 +1,16 @@
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+grades = [[5, 3, 3, 5, 4,], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 print(grades)
-students = {"Jonny", "bilbo", "Steve", "Khendrik", "Aaron"}
+students = ('Jonny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron',)
 print(students)
-A = ["Jonny", 5, 3, 3, 5, 4]
+A = ('Jonny', 5, 3, 3, 5, 4)
 print(A)
-B = ["Bilbo", 2, 2, 2, 3]
+B = ('Bilbo', 2, 2, 2, 3,)
 print(B)
-C = ["Steve", 4, 5, 5, 2]
+C = ('Steve', 4, 5, 5, 2)
 print(C)
-D = ["Khendrik", 4, 4, 3]
+D = ('Khendrik', 4, 4, 3)
 print(D)
-I = ["Aaron", 5, 5, 5, 4, 5]
+I = ('Aaron', 5, 5, 5, 4, 5)
 print(I)
 Jonny = [5, 3, 3, 5, 4]
 average = sum(Jonny) / len(Jonny)
@@ -27,5 +27,4 @@ print(average)
 Aaron = [5, 5, 5, 4, 5]
 average = sum(Aaron) / len(Aaron)
 print(average)
-
-
+print({'Aaron': 4.8, 'Bilbo': 2.25, 'Jonny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.8})
