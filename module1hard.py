717684af2aab42b5dbfb8f85984a5aecbaead8ab
Kurban2132 <k.kokanov@yandex.ru>
2024-10-14 12:23:47 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,30 +1,34 @@
-grades = [[5, 3, 3, 5, 4,], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]  # Исходный список
 print(grades)
-students = ('Jonny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron',)
+students = {"Jonny", "Bilbo", "Steve", "Khendrik", "Aaron"}   # Исходный список
 print(students)
-A = ('Jonny', 5, 3, 3, 5, 4)
+print(sorted(students))  # Список после сортировки
+A = ["Aaron", 5, 5, 5, 4, 5]
 print(A)
-B = ('Bilbo', 2, 2, 2, 3,)
+B = ["Bilbo", 2, 2, 2, 3]
 print(B)
-C = ('Steve', 4, 5, 5, 2)
+C = ["Jonny", 5, 3, 3, 5, 4]
 print(C)
-D = ('Khendrik', 4, 4, 3)
+D = ["Khendrik", 4, 4, 3]
 print(D)
-I = ('Aaron', 5, 5, 5, 4, 5)
+I = ["Steve", 4, 5, 5, 2]
 print(I)
-Jonny = [5, 3, 3, 5, 4]
-average = sum(Jonny) / len(Jonny)
+Aaron = [5, 5, 5, 4, 5]
+average = sum(Aaron) / len(Aaron)
 print(average)
 Bilbo = [2, 2, 2, 3]
 average = sum(Bilbo) / len(Bilbo)
 print(average)
-Steve = [4, 5, 5, 2]
-average = sum(Steve) / len(Steve)
+Jonny = [5, 3, 3, 5, 4]
+average = sum(Jonny) / len(Jonny)
 print(average)
 Khendrik = [4, 4, 3]
 average = sum(Khendrik) / len(Khendrik)
 print(average)
-Aaron = [5, 5, 5, 4, 5]
-average = sum(Aaron) / len(Aaron)
+Steve = [5, 5, 5, 4, 5]
+average = sum(Steve) / len(Steve)
 print(average)
-print({'Aaron': 4.8, 'Bilbo': 2.25, 'Jonny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.8})
+list_1 = ["Aaron-4,8", "Bilbo-2.25", "Jonny-4.0", "Khendrik- 3.66666", "Steve-4.8"]
+print(list_1)
+
